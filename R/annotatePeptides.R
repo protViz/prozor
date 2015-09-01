3d96fc85524c75ad6276e64e42f01aee15a25b7b
Witold Wolski <wewolski@gmail.com>
2015-09-01 18:02:48 +0200

    simplifying usage of and upadate of data

--- R/annotatePeptides.R
+++ R/annotatePeptides.R
@@ -11,6 +11,27 @@
     return(idx)
 }
 
+.getMatchingProteinIDX <- function(data,
+                                   fasta,
+                                   digestPattern = "(([RK])|(^)|(^M))"
+){
+    timeStart <- Sys.time();
+    if( length(data) > 200 & parallel::detectCores(logical=FALSE) > 1){
+        mcCores <- min(6,parallel::detectCores(logical=FALSE))
+        message(paste("going to use : " , mcCores ," cores."))
+        registerDoParallel(mcCores)
+        res <- foreach(i=data ) %dopar% protrazor:::.annotateProteinIDGrep(i, fasta, digestPattern)
+        stopImplicitCluster()
+    }else{
+        res <- lapply(data, .annotateProteinIDGrep, fasta, digestPattern)
+    }
+    names(res) = data
+    timeEnd <- Sys.time();
+    message(paste("time taken: ", difftime(timeEnd, timeStart, units='mins'),  "minutes"))
+    return(res)
+}
+
+
 #' annotate peptides with protein ids
 #' @param data - list of peptides - sequence, optional modified sequence, charge state.
 #' @param fasta - object as created by read.fasta in pacakge seqinr
@@ -20,44 +41,35 @@
 #' library(doParallel)
 #' library(foreach)
 #' data(pepprot)
-#' dim(pepprot)
-#' pepprot<-apply(pepprot,2,as.character)
-#' pepprot <- pepprot[!is.na(pepprot[,1]),]
-#' pepprot <- unique(pepprot[,2:5])
-#' dim(pepprot)
-#' library(seqinr)
+#' head(pepprot)
 #' file = file.path(path.package("protrazor"),"extdata/fgcz_10090_20140715.fasta" )
 #' fasta = read.fasta(file = file, as.string = TRUE, seqtype="AA")
-#' pepseq  = unique(as.character(pepprot[,"peptideSequence"]))
-#' res = annotatePeptides(pepseq, fasta)
-#' length(res)
-#' length(pepseq)
-#' restab = NULL
-#' for(i in res){
-#'  restab = rbind(restab,i)
-#' }
-#' dim(pepprot)
-#' res2 = merge(restab,pepprot,by.x="peptideSequence",by.y="peptideSequence")
-#' dim(res2)
+#' res = annotatePeptides(pepprot, fasta)
+#' head(res2)
 #'
-#'
-annotatePeptides <- function(data,
-                             fasta,
-                             digestPattern = "(([RK])|(^)|(^M))"
+annotatePeptides <- function(pepinfo,
+                                fasta,
+                                digestPattern = "(([RK])|(^)|(^M))"
 ){
-    timeStart <- Sys.time();
-    if( length(data) > 200 & parallel::detectCores(logical=FALSE) > 1){
-        mcCores <- min(6,parallel::detectCores(logical=FALSE))
-        message(paste("going to use : " , mcCores ," cores."))
-        registerDoParallel(mcCores)
-        data <- foreach(i=data ) %dopar% protrazor:::.annotateProteinIDGrep(i, fasta, digestPattern)
-        stopImplicitCluster()
-    }else{
-        data <- lapply(data, .annotateProteinIDGrep, fasta, digestPattern)
+    pepinfo = apply(pepinfo,2,as.character)
+    lengthPeptide = sapply(pepinfo[,"peptideSequence"],nchar)
+    pepinfo = cbind(pepinfo,"lengthPeptide"=lengthPeptide)
+
+    pepseq  = unique(as.character(pepinfo[,"peptideSequence"]))
+    res = protrazor:::.getMatchingProteinIDX(pepseq, fasta,digestPattern)
+    lengthFasta  = sapply(fasta,nchar)
+    namesFasta = names(fasta)
+    protLength = vector(length(res),mode="list")
+    for(i in 1:length(res)){
+        protLength[[i]] =rbind("lengthProtein"=lengthFasta[res[[i]]],"proteinID"=namesFasta[res[[i]]],"peptideSequence"=names(res)[i])
     }
-    timeEnd <- Sys.time();
-    message(paste("time taken: ", difftime(timeEnd, timeStart, units='mins'),  "minutes"))
-    return(data)
+    restab = matrix(unlist(protLength),ncol=3,byrow=TRUE)
+    colnames(restab) = c("lengthProtein","proteinSequence","peptideSequence")
+    restab <<- restab
+    pepinfo <<- pepinfo
+    res = merge(restab,pepinfo,by.x="peptideSequence",by.y="peptideSequence")
+
+    return(res)
 }
 
 
--- R/prepareMatrix.R
+++ R/prepareMatrix.R
@@ -1,11 +1,16 @@
+.makePeptideID <- function(peprot){
+    peprot[,c("peptideModSequence","z")]
+
+}
 #' given table of peptide protein assigments generate matrix
 #' @param data  column 1 proteins, column 2 peptides
 #' @value sparse matrix
 #' @export
 prepareMatrix <- function(data){
-    fprots = as.factor(data[,"prots"])
+
+    fprots = as.factor(data[,"proteinID"])
     prots = as.integer(fprots)
-    fpeps = as.factor(data[,"peps"])
+    fpeps = as.factor(data[,"peptideSequence"])
     peps = as.integer(fpeps)
 
     pepProt =sparseMatrix(peps , prots,x = 1 )
