a62774e2c09cb6bb2c35119bf2b2cfb78394c6df
Witold Wolski <wewolski@gmail.com>
2016-04-21 18:43:45 +0200

    removed old code

--- R/greedy.R
+++ R/greedy.R
@@ -32,44 +32,6 @@
 }
 
 
-.greedy <- function( pepprot ){
-    ncolX = ncol(pepprot)
-    res<-vector(ncolX , mode="list")
-    idxx <-NULL
-    for(i in 1:ncolX)
-    {
-        if(i %% 10 == 0){
-            pepprot <- .removeZeroRows(pepprot)
-            pepprot <- pepprot[,-idxx,drop=FALSE]
-            idxx <-NULL
-            message(paste("new dim" , dim(pepprot)))
-        }
-        if(nrow(pepprot) == 0){
-            return(res[1:(i-1)])
-        }
-        oldtime <- Sys.time()
-        pepsPerProt <- colSums(pepprot)
-        if(max(pepsPerProt) == 0){
-            return(res[1:(i-1)])
-        }
-        idx <- which.max(pepsPerProt)
-        if(length(idx) > 1){
-            idx<-idx[1]
-        }
-        idxx <- c(idxx, idx )
-        dele <- pepprot[,idx]
-        tmpRes = list(prot = colnames(pepprot)[idx], peps = rownames(pepprot)[dele > 0])
-        res[[i]] <- tmpRes
-        #message(paste(i, " protein : ", colnames(pepprot)[idx], " matched by peptides: ", sum(dele)))
-        if(sum(dele) > 0){
-            set = cbind(rep(dele > 0, ncol(pepprot)))
-            pepprot[set] <- 0
-        }
-        newtime <- Sys.time()
-        message(paste("time ",newtime - oldtime))
-    }
-    return(res)
-}
 #' given matrix (columns protein rows peptides), compute minimal protein set using greedy algorithm
 #' @param pepprot matrix as returned by prepareMatrix
 #' @return list of peptide protein assignment
@@ -87,7 +49,9 @@
 #' stopifnot(length(unique(names(es))) == dim(unique(protpepmetashort[,4:5]))[1])
 #'
 greedy <- function( pepprot ){
+    print("here")
     protPepAssingments <- .greedy2(pepprot)
+    print("passed")
     matrixlist <- lapply(protPepAssingments,function(x){ t(cbind(x$peps, rep(x$prot,length(x$peps)))) })
     res = matrix(unlist(matrixlist), ncol=2, byrow = TRUE)
     ltmp = as.list(res[,2])
