dbd7f418ba9ad559e21b290de962f787edb6515f
Christian Panse <cpanse@users.noreply.github.com>
2017-03-22 17:03:34 +0100

    Create zzz.R

---
+++ R/zzz.R
@@ -0,0 +1,10 @@
+#R
+
+.onAttach <- function(lib, pkg){
+	if(interactive()){
+		version <- packageVersion('prozor')
+		packageStartupMessage("Package 'prozor' version ", version)
+		# packageStartupMessage("Type 'citation(\"prozor\")' for citing this R package in publications.")
+	  invisible()
+	}
+}
