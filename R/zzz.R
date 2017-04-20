d73941a9e8c8a4f25ceec37ac156dae7db4bb964
Christian Panse <cp@fgcz.ethz.ch>
2017-04-20 11:11:40 +0200

    0.2.3 cpanse@fgcz-r-028:~ > /scratch/cpanse/R/R-devel/bin/R CMD check __checkouts/prozor_0.2.3.tar.gz  --as-cran * using log directory ‘/home/cpanse/prozor.Rcheck’ * using R Under development (unstable) (2017-04-19 r72556) * using platform: x86_64-pc-linux-gnu (64-bit) * using session charset: UTF-8 * using option ‘--as-cran’ * checking for file ‘prozor/DESCRIPTION’ ... OK * checking extension type ... Package * this is package ‘prozor’ version ‘0.2.3’ * checking CRAN incoming feasibility ... Note_to_CRAN_maintainers Maintainer: ‘Witold Wolski <wewolski@gmail.com>’ * checking package namespace information ... OK * checking package dependencies ... OK * checking if this is a source package ... OK * checking if there is a namespace ... OK * checking for executable files ... OK * checking for hidden files and directories ... OK * checking for portable file names ... OK * checking for sufficient/correct file permissions ... OK * checking whether package ‘prozor’ can be installed ... OK * checking installed package size ... OK * checking package directory ... OK * checking DESCRIPTION meta-information ... OK * checking top-level files ... OK * checking for left-over files ... OK * checking index information ... OK * checking package subdirectories ... OK * checking R files for non-ASCII characters ... OK * checking R files for syntax errors ... OK * checking whether the package can be loaded ... OK * checking whether the package can be loaded with stated dependencies ... OK * checking whether the package can be unloaded cleanly ... OK * checking whether the namespace can be loaded with stated dependencies ... OK * checking whether the namespace can be unloaded cleanly ... OK * checking loading without being on the library search path ... OK * checking use of S3 registration ... OK * checking dependencies in R code ... OK * checking S3 generic/method consistency ... OK * checking replacement functions ... OK * checking foreign function calls ... OK * checking R code for possible problems ... OK * checking Rd files ... OK * checking Rd metadata ... OK * checking Rd line widths ... OK * checking Rd cross-references ... OK * checking for missing documentation entries ... OK * checking for code/documentation mismatches ... OK * checking Rd \usage sections ... OK * checking Rd contents ... OK * checking for unstated dependencies in examples ... OK * checking contents of ‘data’ directory ... OK * checking data for non-ASCII characters ... OK * checking data for ASCII and uncompressed saves ... OK * checking examples ... OK * checking PDF version of manual ... OK * DONE
    Status: OK

--- R/zzz.R
+++ R/zzz.R
@@ -2,7 +2,7 @@
 
 .onAttach <- function(lib, pkg){
 	if(interactive()){
-		version <- packageVersion('prozor')
+		version <- utils::packageVersion('prozor')
 		packageStartupMessage("Package 'prozor' version ", version)
 		# packageStartupMessage("Type 'citation(\"prozor\")' for citing this R package in publications.")
 	  invisible()
