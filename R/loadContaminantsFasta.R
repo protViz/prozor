b3b62b2b4e05bb58491cdd109ba25b716fee7241
Witold Wolski <wewolski@gmail.com>
2019-11-05 10:17:10 +0100

    added new contaminants 2019

--- R/loadContaminantsFasta.R
+++ R/loadContaminantsFasta.R
@@ -25,3 +25,16 @@ loadContaminantsNoHumanFasta <- function(){
     contaminants <- readPeptideFasta(file)
 }
 
+#' load list of contaminant sequences FGCZ 2019
+#'
+#' @export
+#' @examples
+#' library(prozor)
+#' cont <- loadContaminantsFasta2019()
+#' cont[[1]]
+#' #example how to create a protein db with decoy sequences
+loadContaminantsFasta2019 <- function(){
+  file = system.file("extdata/fgcz_contaminants2019_20190708.fasta.gz",package = "prozor")
+  #file = file.path(path.package("prozor"),"extdata/fgcz_ContaminantsWithAnnotation.fasta.gz")
+  contaminants <- readPeptideFasta(file)
+}
