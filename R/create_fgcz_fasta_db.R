da98915e2a00fae61c1bd4c00d78d2a0e516a341
wolski <wewolski@gmail.com>
2020-06-04 12:01:59 +0200

    create fasta script

--- R/create_fgcz_fasta_db.R
+++ R/create_fgcz_fasta_db.R
@@ -1,7 +1,10 @@
 #' create fasta db from one or more fasta files
 #' @export
 #'
-create_fgcz_fasta_db <- function(databasedirectory , useContaminants = loadContaminantsFasta2019(), revLab = "REV_"){
+create_fgcz_fasta_db <- function(databasedirectory ,
+                                 useContaminants = loadContaminantsFasta2019(),
+                                 revLab = "REV_",
+                                 outputdir = NULL){
     mcall <- match.call()
 
     dir.exists(databasedirectory)
@@ -10,18 +13,24 @@ create_fgcz_fasta_db <- function(databasedirectory , useContaminants = loadConta
     fasta <- grep("fasta", dir(databasedirectory), value = T)
     files1 <- file.path(databasedirectory, fasta)
     annot <- grep("annotation",dir(databasedirectory), value = T)
-
-    annotation <- readLines(file.path(databasedirectory,annot))
+    if(length(annot) == 0) { stop("NO annotation file found") }
+    annotation <- readLines(file.path(databasedirectory, annot))
 
     resDB <- createDecoyDB(files1,useContaminants = useContaminants, annot = annotation, revLab = revLab)
     resDB <- resDB[!duplicated(names(resDB))]
 
-    if (is.null(revLab)) {
-        filepath <- file.path(dirname(databasedirectory), paste(dbname,"_",format(Sys.time(), "%Y%m%d"),".fasta" ,sep = ""))
-    } else {
-        dbname_decoy <- paste0(dbname,"_d")
-        filepath <- file.path(dirname(databasedirectory), paste(dbname_decoy,"_",format(Sys.time(), "%Y%m%d"),".fasta" ,sep = ""))
+    if (is.null(outputdir)) {
+        outputdir <- databasedirectory
     }
+
+    if (is.null(revLab)) {
+        filepath <- file.path(dirname(outputdir), paste(dbname,"_",format(Sys.time(), "%Y%m%d"),".fasta" ,sep = ""))
+    } else {
+        dbname <- paste0(dbname,"_d")
+        filepath <- file.path(dirname(outputdir), paste(dbname,"_",format(Sys.time(), "%Y%m%d"),".fasta" ,sep = ""))
+    }
+
+
     writeFasta(resDB, file = filepath)
 
     {
@@ -30,19 +39,21 @@ create_fgcz_fasta_db <- function(databasedirectory , useContaminants = loadConta
 
         aafreq <- table(vec)
         aafreq <- paste(capture.output(as.matrix(aafreq)),"\n", sep = "")
-        length_s <- summary(sapply(resDB, getLength))
+        length_s <- summary(sapply(resDB, seqinr::getLength))
         length_s <- paste(capture.output(length_s),"\n", sep = "")
 
 
         summary <- paste0(
-            "Database created with prozor: ", capture.output(mcall), "\n",
+            "Database created with prozor: ", paste(capture.output(mcall), collapse = "\n"), "\n",
             "where databasedirectory was prepared according to https://fgcz-intranet.uzh.ch/tiki-index.php?page=SOPrequestFASTA \n\n",
-            "FASTA name: ",basename(filepath),
-            "\nannotation:\n",
+            "\n      FASTA name : ", dbname,
+            "\n FASTA file name : ", basename(filepath),
+            "\n  written to dir : ", outputdir,
+            "\n       annotation:\n",
             annotation, "\n",
-            "nr sequences: " , length(resDB), "\n")
+            "\n      nr sequences: " , length(resDB), "\n")
         summary <- c(summary, "length summary:\n", length_s, "AA frequencies:\n", aafreq)
 
     }
-    return( list(resDB = resDB, filepath = filepath, summary = summary,mcall = mcall))
+    return( list(resDB = resDB, filepath = filepath, summary = summary, mcall = mcall, dbname = dbname))
 }
---
+++ inst/script/fgcz_create_fasta.R
@@ -0,0 +1,67 @@
+#!/usr/bin/Rscript
+
+library(docopt)
+library(prozor)
+
+doc <- "Sample Size Report from MQ file
+
+Usage:
+  fgcz_create_fasta.R <fasta_dir> [--contamin=<contamin>] [--revLab=<revLab>] [--output_dir=<output_dir>]
+  fgcz_create_fasta.R <fasta_dir> nodecoy [--contamin=<contamin>] [--output_dir=<output_dir>]
+
+
+Options:
+  -o --output_dir=<output_dir> output directory, default next to fasta_dir
+  -c --contamin=<contamin> add contaminants [default: fgcz2019] or `none` or path to fasta file with contaminants
+  -r --revLab=<revLab> create reverse sequences with prefix [default: REV_], empty string no decoys.
+
+
+Arguments:
+  fasta_dir input file
+"
+
+print(commandArgs(TRUE))
+if (FALSE) {
+    args <- c("C:\\Users\\wewol\\Dropbox\\DataAnalysis\\p65\\fgcz_9606_SARS_CoV_2_reviewed_cnl")
+    #args <- c("C:\\Users\\wewol\\Dropbox\\DataAnalysis\\p65\\fgcz_10116_RattusNor_reviewed_cnl")
+    opt <- docopt(doc,args = args)
+}else{
+    opt <- docopt(doc)
+}
+
+
+params <- c("\nParameters used:\n\t",
+    " fasta_dir: ", fasta_dir <- opt$fasta_dir, "\n\t",
+    "   nodecoy: ",   nodecoy <- opt$nodecoy, "\n\t",
+    "  contamin: ", contamin <- opt[["--contamin"]], "\n\t",
+    "    revLab: ", revLab <- opt[["--revLab"]], "\n\t",
+    "output_dir: ", output_dir <- opt[["--output_dir"]], "\n\n\n\t")
+
+cat(paste(params, collapse = ""))
+
+if (contamin == "fgcz2019") {
+    contamin <- loadContaminantsFasta2019()
+}else if (contamin == "none") {
+    contamin <- NULL
+}else{
+    if (file.exists(contamin)) {
+        contamin <- prozor::readPeptideFasta(contamin)
+    }
+}
+
+if (nodecoy) {
+    revLab <- NULL
+}
+
+#source("c:/Users/wewol/prog/prozor/R/create_fgcz_fasta_db.R")
+
+resDB <-  create_fgcz_fasta_db(fasta_dir,
+                               useContaminants = contamin,
+                               revLab = revLab,
+                               outputdir = output_dir)
+
+cat(resDB$summary)
+
+res <- paste(c("fgcz_create_fasta.R run with params\n",params),collapse = "")
+writeLines(c(res, paste(resDB$summary, collapse="")), con = paste0(resDB$dbname,".txt"))
+
