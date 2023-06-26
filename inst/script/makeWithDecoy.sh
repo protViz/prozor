f7296ba78b84d99b10542cfdc7982c1edaf34ab6
Witold Wolski <wewolski@gmail.com>
2023-06-26 09:56:08 +0200

    add --summary option as default

--- inst/script/makeWithDecoy.sh
+++ inst/script/makeWithDecoy.sh
@@ -1,7 +1,7 @@
 set -t
 echo $1
 export R_LIBS_SITE="/scratch/LIBWITHPROZOR/r-site-library/"
-Rscript --vanilla $R_LIBS_SITE/prozor/script/fgcz_create_fasta.R $1 -o /srv/www/htdocs/FASTA/
+Rscript --vanilla $R_LIBS_SITE/prozor/script/fgcz_create_fasta.R $1 --summary -o /srv/www/htdocs/FASTA/
 echo "---------"
 #echo "$(<$1_d.txt)"
 fastastore=$(ls /srv/www/htdocs/FASTA/$1_d*)
